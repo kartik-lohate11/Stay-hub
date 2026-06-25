ae1b730edff0491315d005c34ec22347c71eb965
kartik-lohate11 <kartiklohteofficial@gmail.com>
2026-06-25 15:16:03 +0530

    add function at userService

--- src/main/java/com/stay/hub/service/impl/UserServiceImpl.java
+++ src/main/java/com/stay/hub/service/impl/UserServiceImpl.java
@@ -1,4 +1,12 @@
 package com.stay.hub.service.impl;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class UserServiceImpl {
+
+    public void hello(){
+System.out.println("I am god");
+    }
+
 }
