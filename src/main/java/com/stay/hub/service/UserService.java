a65af9d6cde9288b1beeee04ddfcd910a6a19861
kartik-lohate11 <kartiklohteofficial@gmail.com>
2026-06-25 15:04:25 +0530

    push initial code

---
+++ src/main/java/com/stay/hub/service/UserService.java
@@ -0,0 +1,7 @@
+package com.stay.hub.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService {
+}
---
+++ src/main/java/com/stay/hub/service/impl/UserServiceImpl.java
@@ -0,0 +1,4 @@
+package com.stay.hub.service.impl;
+
+public class UserServiceImpl {
+}
