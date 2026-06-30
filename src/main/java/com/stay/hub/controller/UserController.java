1ab0da85510b1fcaed19b275f6681bb62a1b6aba
kartik-lohate11 <kartiklohteofficial@gmail.com>
2026-06-30 15:42:19 +0530

    Apply .gitignore

--- src/main/java/com/stay/hub/controller/UserController.java
+++ src/main/java/com/stay/hub/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.stay.hub.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@Slf4j
 public class UserController {
 }
